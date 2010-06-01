0924ff14b7203d37eeaf4f02b66fc1734a40cbe4
theosp <333222@gmail.com>
2010-06-01 03:15:11 +0300

    Add first version of the modules dict, file, object, string, test + test files for them

---
+++ tests/test_object.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+BUPLER_LIB_PATH="../src/bupler_lib/"
+. ../src/bupler
+
+bupler.import object
+bupler.import test
+
+object.Object daniel
+daniel.set_attr daniel 2
+daniel.daniel
+daniel.daniel
+
+object.__type__
+daniel.__type__
