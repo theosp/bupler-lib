1c59d2b2a39df71b38e9fffa5112d02cfe3c48c2
theosp <333222@gmail.com>
2010-06-13 12:29:26 +0300

    Add development mode to the setup script.

--- setup.sh
+++ setup.sh
@@ -7,6 +7,10 @@ else
     . setup_settings.default # load the default setup_settings.
 fi
 
+# load the bupler module for the bupler.script_pwd()
+. modules/bupler
+script_pwd=$(bupler.script_pwd)
+
 cat <<EOF
 Bupler Setup
 ============
@@ -77,6 +81,39 @@ EOF
     fi
 fi
 
+DEV_MODE=0
+echo -n "Do you want to install bupler in development mode? [y/N/h] type h for help "
+read -n1 ans
+echo
+if [[ "$ans" = [yYhH] ]]
+then
+    if [[ "$ans" = [hH] ]]
+    then
+        cat <<EOF
+
+Development Mode
+----------------
+
+In contrast to the regular setup, installing bupler in development mode creates
+symbolic link to the modules folder in this directory on $LIB_PATH instead of
+coping it there. That way you won't have to reinstall Bupler after pulling new
+version, switching between branches or making changes.
+
+But you'll have to remember not to remove this directory, otherwise you won't
+be able to load Bupler.
+
+EOF
+
+        echo -n "Do you want to install bupler in dev mode? [y/N] "
+        read -n1 ans
+        echo
+    fi
+    if [[ "$ans" = [yY] ]]
+    then
+        DEV_MODE=1
+    fi
+fi
+
 echo -n "Ready to install Bupler? [y/N] "
 read -n1 ans
 echo
@@ -85,7 +122,12 @@ then
     exit 1
 fi
 
-echo " * Copy modules to $LIB_PATH"
+if (( $DEV_MODE ))
+then
+    echo " * Create symlink to the modules directory: $script_pwd/modules -> $LIB_PATH"
+else
+    echo " * Copy modules to $LIB_PATH"
+fi
 if [[ -e "$LIB_PATH" || -L "$LIB_PATH" ]]
 then
     echo -n "$LIB_PATH already exists, delete it? [y/N] "
@@ -98,7 +140,13 @@ then
         exit 1
     fi
 fi
-cp -r modules $LIB_PATH
+
+if (( $DEV_MODE ))
+then
+    ln -s "$script_pwd/modules" $LIB_PATH
+else
+    cp -r modules $LIB_PATH
+fi
 
 BUPLER_MODULE_SYMLINK="$BUPLER_MODULE_SYMLINK_DIR/bupler"
 echo " * Create symlink "$BUPLER_MODULE_SYMLINK' -> '$LIB_PATH/bupler
